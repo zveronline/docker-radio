4533270dd0599b0209d1cad778f8b4a6877ff7b4
zveronline <zveronline@zveronline.ru>
2020-08-26 11:00:39 +0300

    add env

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM alpine:3.12
 
 ENV ROMPR_VERSION=1.48 \
 ADMIN_PASSWORD=qwe123test \
-BITRATE="256"
+BITRATE=256 \
+ENCODER=ogg \
+MOUNT=radio.ogg
 
 ADD conf /conf
 ADD entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ mkdir /var/lib/mpd/playlists
 fi
 
 sed -i s/ADMIN_PASSWORD/$ADMIN_PASSWORD/g /conf/icecast.xml
+sed -i s/BITRATE/$BITRATE/g /conf/radio.conf
+sed -i s/ENCODER/$ENCODER/g /conf/radio.conf
+sed -i s/MOUNT/$MOUNT/g /conf/radio.conf
 
 exec "$@"
