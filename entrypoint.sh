f3a6812c48fed580755d06a554d0758a5d12880f
zveronline <zveronline@zveronline.ru>
2020-08-27 08:46:36 +0300

    add some environments

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ mkdir /var/lib/mpd/playlists
 fi
 
 sed -i s/ADMIN_PASSWORD/$ADMIN_PASSWORD/g /conf/icecast.xml
+sed -i s/NAMESTREAM/$NAMESTREAM/g /conf/radio.conf
+sed -i s/DESCRIPTION/$DESCRIPTION/g /conf/radio.conf
+sed -i s/URL/$URL/g /conf/radio.conf
 sed -i s/BITRATE/$BITRATE/g /conf/radio.conf
 sed -i s/ENCODER/$ENCODER/g /conf/radio.conf
 sed -i s/MOUNT/$MOUNT/g /conf/radio.conf
